d80b5af816156f1fea5860433425ff09cc4f74bd
lvalido <lvalido@mailexternos.com.ar>
2016-11-24 11:47:29 -0300

    [Scraper] AddMulti Stock

--- server.js
+++ server.js
@@ -1,37 +1,25 @@
 var express = require('express');
-var request = require('request');
+var request = require('sync-request');
+var $ = require('cheerio');
 var app     = express();
-var fs = require('fs')
+var fs = require('fs');
 
 var port = process.argv[2] || 8082;
-var actions = actions = fs.readFileSync('actions.js', 'utf8');
+var rawData = fs.readFileSync('actions.js', 'utf8');
 
+var actions = [
+  {name:'fran'},
+  {name:'mirg'},
+  {name:'ggal'}
+  ];
 app.get('/scrape', function(req, res){
-  for (var i= 0; i <actions.length; i ++) {
+  var htmlOutput ='';
+  for (var i = 0; i < actions.length; i++) {
     var action = actions[i];
-    //All the web scraping magic will happen here
-    url = 'https://www.google.com/finance/info?q=BCBA:' + action.name;
-
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
-    request(url, function(error, response, html){
-        // First we'll check to make sure no errors occurred when making the request
-        if(!error){
-          console.log(url);
-          console.log(html);
-          var rawAnswer = html.replace('//','');
-            console.log(rawAnswer);
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-            var answer = JSON.parse(rawAnswer);
-            res.send(answer[0].l)
-        }else{
-          console.log('An error was ocurred');
-        }
-    });
+    var answer = getPrice(action.name);
+   htmlOutput += answer.name + ': ' + answer.price + '<BR/> ';
   }
-
-    
+  res.send(htmlOutput);
 });
 
 app.listen(port);
@@ -39,3 +27,14 @@ app.listen(port);
 console.log('Listening on ' + port);
 
 exports = module.exports = app;
+
+function getPrice(ticket) {
+    var url = 'https://www.google.com/finance/info?q=BCBA:' + ticket;
+    
+    var html = request('GET',url);
+    var rawAnswer = html.getBody('utf8').replace('//','');
+    
+    var answer = JSON.parse(rawAnswer);
+    var returnValue = {name: answer[0].t, price: answer[0].l};
+    return returnValue;
+}
